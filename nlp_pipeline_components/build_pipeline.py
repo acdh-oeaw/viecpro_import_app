d922e3dc64af4cffa4344bd9ef20026f82b6c42b
gregorpirgie <gregor.pirgie@univie.ac.at>
2021-06-11 22:31:15 +0200

    removed entity ruler from components

--- nlp_pipeline_components/build_pipeline.py
+++ nlp_pipeline_components/build_pipeline.py
@@ -18,10 +18,13 @@ def get_model(annotations=False, cfg=None):
     if not cfg:
         cfg = run_hardcode()
 
-    nlp = spacy.load("models/viecpro_ner_hzab_12-20/")
+    #nlp = spacy.load("models/viecpro_ner_hzab_12-20/")
+    nlp = spacy.load("models/viecpro_ner_hsv_5-21/")
+    print("using this model: ", nlp)
 
 
-    use_existing_annotations = UseExistingAnnotations(nlp, "data/viecpro_HZAB_funktion_0.jsonl")
+    #use_existing_annotations = UseExistingAnnotations(nlp, "data/viecpro_HZAB_funktion_0.jsonl")
+    use_existing_annotations = UseExistingAnnotations(nlp, "data/viecpro_HSV_0.jsonl")
     add_brackets = AddBrackets(nlp)
     rename_functions = RenameFunctions(nlp, lst_hofst=cfg.lst_hofst, df_aemter_index=cfg.df_aemter_index)
     date_prepocissions = DatePrepocissions(nlp)
@@ -33,8 +36,8 @@ def get_model(annotations=False, cfg=None):
     if annotations:
         nlp.add_pipe(use_existing_annotations)
 
-    nlp.add_pipe(extend_func_ents, first=True)
-    nlp.add_pipe(ruler, first=True)
+    #nlp.add_pipe(extend_func_ents, first=True)
+    #nlp.add_pipe(ruler, first=True)
     nlp.add_pipe(add_brackets)
     nlp.add_pipe(rename_functions)
     nlp.add_pipe(remove_names)
